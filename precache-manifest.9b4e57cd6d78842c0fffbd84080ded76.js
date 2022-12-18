7f7919a8a2fd0e493cb38e1a051b1fa5119c964d
shrikantkumar1999 <shrikantkumar16121998@gmail.com>
2022-12-18 17:20:55 +0530

    Updates

---
+++ precache-manifest.9b4e57cd6d78842c0fffbd84080ded76.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30182e40dc1ce6826928eeeefa66f93d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "77b8f06a0f608bc7d09f",
+    "url": "/portfolio/static/css/main.49bb6bd9.chunk.css"
+  },
+  {
+    "revision": "6ddca471a0a253f982dd",
+    "url": "/portfolio/static/js/2.48a0a233.chunk.js"
+  },
+  {
+    "revision": "e74be44262859c6bbcbe953b532630d6",
+    "url": "/portfolio/static/js/2.48a0a233.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77b8f06a0f608bc7d09f",
+    "url": "/portfolio/static/js/main.98d4a960.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "b82ec4ea99b656cb94d2be7bd0b2d1bb",
+    "url": "/portfolio/static/media/codeforces.b82ec4ea.jpg"
+  },
+  {
+    "revision": "ea69a3b82520172805c412123d1c97db",
+    "url": "/portfolio/static/media/error404.ea69a3b8.jpg"
+  },
+  {
+    "revision": "398f840c1638d3de3ba5918fc52d21e0",
+    "url": "/portfolio/static/media/github.398f840c.png"
+  },
+  {
+    "revision": "65bdba157f6fd1534701dd04bd1cd4e7",
+    "url": "/portfolio/static/media/me.65bdba15.jpg"
+  }
+]);
